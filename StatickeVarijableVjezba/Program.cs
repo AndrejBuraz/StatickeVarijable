b8c1f410c5e765713eaa2e02f85441c7f706f333
Andrej <75538726+AndrejBuraz@users.noreply.github.com>
2022-11-23 09:18:27 +0100

    Add files via upload

---
+++ StatickeVarijableVjezba/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatickeVarijableVjezba
+{
+    class PrvaKlasa
+    {
+        private static int counter;
+
+        public PrvaKlasa()
+        {
+            counter++;
+        }
+        public int Counter()
+        {
+            return counter;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrvaKlasa x = new PrvaKlasa();
+            PrvaKlasa y = new PrvaKlasa();
+            PrvaKlasa z = new PrvaKlasa();
+            Console.WriteLine("Vrijednost staticke varijable counter: " + x.Counter());
+            Console.ReadKey();
+        }
+    }
+}
